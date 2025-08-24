f2364c80dca05bfa60cc678cccaf8ae07989e70d
JasonPaff <jasonpaff@gmail.com>
2025-08-24 11:54:24 -0400

    fix drizzle import

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,5 @@
 import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 
 import * as schema from './schema/index';
 
