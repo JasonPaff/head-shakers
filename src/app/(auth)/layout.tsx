4283319abb5028c6b40302781211b062cbac040b
JasonPaff <jasonpaff@gmail.com>
2025-08-24 14:14:34 -0400

    clerk sign-in & sign-up pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,24 +1,5 @@
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
-
-import { Button } from '@/components/ui/button';
-
 type AuthLayoutProps = Children;
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-  return (
-    <ClerkProvider>
-      <header>
-        <SignedOut>
-          <SignInButton mode={'redirect'} />
-          <SignUpButton mode={'redirect'}>
-            <Button>Sign Up</Button>
-          </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-      {children}
-    </ClerkProvider>
-  );
+  return <div>{children}</div>;
 }
