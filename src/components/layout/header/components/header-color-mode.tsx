d85ad92898150f8ad367446b8ccca4dd5e2466e0
JasonPaff <jasonpaff@gmail.com>
2025-08-24 17:55:36 -0400

    app layout

--- src/components/layout/header/components/header-color-mode.tsx
+++ src/components/layout/header/components/header-color-mode.tsx
@@ -31,11 +31,11 @@ export const HeaderColorMode = () => {
       <DropdownMenuTrigger asChild>
         <Button size={'icon'} variant={'outline'}>
           <SunIcon
-            className={'h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:-rotate-0'}
+            className={'h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90'}
           />
           <MoonIcon
             className={
-              'absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90'
+              'absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:-rotate-0'
             }
           />
           <span className={'sr-only'}>Toggle theme</span>
