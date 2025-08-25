5ff4d963c24cb2ece3dec82892cea3ebba6ed73b
JasonPaff <jasonpaff@gmail.com>
2025-08-24 22:25:48 -0400

    remove sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,5 @@
-import { AppSidebar } from '@/components/layout/app-sidebar';
-import { SidebarInset } from '@/components/ui/sidebar/sidebar-inset';
-import { SidebarProvider } from '@/components/ui/sidebar/sidebar-provider/sidebar-provider';
-
 type AppLayoutProps = LayoutProps;
 
 export default function AppLayout({ children }: AppLayoutProps) {
-  return (
-    <SidebarProvider isDefaultOpen>
-      <AppSidebar />
-      <SidebarInset>
-        <div className={'flex min-h-screen flex-1 flex-col gap-4 p-4'}>{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <>{children}</>;
 }
