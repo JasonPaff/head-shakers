7c2784c2568d9713f69a9c02db2af4c12b1198cf
JasonPaff <jasonpaff@gmail.com>
2025-11-05 13:49:04 -0500

    cloudinary image host added to config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   experimental: {
     browserDebugInfoInTerminal: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default withSentryConfig(nextConfig, {
