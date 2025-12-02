d5e2313abf156b019ec8a492552f5ab78547423a
JasonPaff <jasonpaff@gmail.com>
2025-12-02 15:29:16 -0500

    feat: smooth scroll to top on page change in bobblehead grid display

--- src/app/(app)/dashboard/collection/(collection)/components/display/bobblehead-grid-display.tsx
+++ src/app/(app)/dashboard/collection/(collection)/components/display/bobblehead-grid-display.tsx
@@ -77,6 +77,7 @@ export const BobbleheadGridDisplay = ({
 
   const handlePageChange = (newPage: number) => {
     void setParams({ page: newPage });
+    window.scrollTo({ behavior: 'smooth', top: 0 });
   };
 
   const handlePageSizeChange = (newPageSize: number) => {
