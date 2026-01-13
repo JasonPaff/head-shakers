172ce8ccd9b6b5f62e60c4bfab6e008fa2868a5f
JasonPaff <jasonpaff@gmail.com>
2026-01-12 22:01:35 -0500

    refactor: remove redundant ESLint directives in upload signature API route
    - Eliminated unnecessary `eslint-disable` comments for cleaner code.

--- src/app/api/upload/sign/route.ts
+++ src/app/api/upload/sign/route.ts
@@ -1,12 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment  */
+/* eslint-disable @typescript-eslint/no-unsafe-argument  */
 import { CloudinaryService } from '@/lib/services/cloudinary.service';
 
 export async function POST(request: Request) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const body = await request.json();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { paramsToSign } = body;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const signature = CloudinaryService.generateUploadSignature(paramsToSign);
 
   return Response.json({ signature });
